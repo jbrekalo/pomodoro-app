3b26498bf173909ed8a1a5af949597ce3361328c
jbrekalo <josip.brekalo@hotmail.com>
2025-04-15 20:29:11 +0200

    callback instead emitter

--- src/main.js
+++ src/main.js
@@ -1,24 +1,24 @@
-class Timer extends EventTarget {
-  constructor() {
-    super();
-
+class Timer {
+  constructor(onTick) {
     this.state = {
       running: false,
       minutes: 25,
       seconds: 0,
     };
+    this.onTick = onTick;
+    this.interval = null;
   }
 
   start() {
+    if (this.state.running) return;
+
     this.state.running = true;
 
-    const countdown = setInterval(() => {
-      const formattedMinutes = String(this.state.minutes).padStart(2, "0");
-      const formattedSeconds = String(this.state.seconds).padStart(2, "0");
-
+    this.interval = setInterval(() => {
       if (this.state.seconds === 0) {
         if (this.state.minutes === 0) {
-          clearInterval(countdown);
+          clearInterval(this.interval);
+          this.state.running = false;
           return;
         }
         this.state.minutes--;
@@ -26,54 +26,66 @@ class Timer extends EventTarget {
       } else {
         this.state.seconds--;
       }
-      this.timeLeft = `${formattedMinutes}:${formattedSeconds}`;
-      this.dispatchEvent(new CustomEvent("tick", { detail: this.timeLeft }));
+
+      const formattedMinutes = String(this.state.minutes).padStart(2, "0");
+      const formattedSeconds = String(this.state.seconds).padStart(2, "0");
+
+      this.onTick(`${formattedMinutes}:${formattedSeconds}`);
     }, 1000);
   }
 
-  pause() {}
+  pause() {
+    clearInterval(this.interval);
+    this.state.running = false;
+  }
 
-  reset() {}
+  reset() {
+    clearInterval(this.interval);
+    this.state = {
+      running: false,
+      minutes: 25,
+      seconds: 0,
+    };
+    this.onTick("25:00");
+  }
 }
 
 class UIController {
-  constructor(timer) {
+  constructor() {
     this.allTabs = document.querySelectorAll("[aria-selected]");
     this.timerLabel = document.querySelector(".timer");
     this.controlBtn = document.getElementById("control-button");
-    this.timer = timer;
+
+    this.timer = new Timer((timeStr) => {
+      this.timerLabel.textContent = timeStr;
+    });
   }
 
-  setupEventListners() {
+  setupEventListeners() {
     this.allTabs.forEach((tab) => {
       tab.addEventListener("click", () => {
         this.allTabs.forEach((tab) =>
           tab.setAttribute("aria-selected", "false")
         );
-
         tab.setAttribute("aria-selected", "true");
       });
     });
 
     this.controlBtn.addEventListener("click", () => {
       this.timer.start();
-      this.timer.addEventListener(
-        "tick",
-        (e) => (this.timerLabel.textContent = e.detail)
-      );
     });
   }
 
-  updateScreen() {}
+  initTime() {
+    this.timerLabel.textContent = "25:00";
+  }
 }
 
 class App {
-  constructor() {}
   init() {
-    const timer = new Timer();
-    const controller = new UIController(timer);
-    controller.updateScreen();
-    controller.setupEventListners();
+    const controller = new UIController();
+    controller.initTime();
+    controller.setupEventListeners();
   }
 }
 
