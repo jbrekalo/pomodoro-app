92cc21309da6d9c4974fc74c7c5a13daf72b2f09
jbrekalo <josip.brekalo@hotmail.com>
2025-04-18 21:10:17 +0200

    advanced timer

--- src/main.js
+++ src/main.js
@@ -1,52 +1,63 @@
 class Timer {
   constructor(onTick) {
-    this.initialTime = {
-      minutes: 25,
-      seconds: 0,
-    };
     this.state = {
       running: false,
-      minutes: this.initialTime.minutes,
-      seconds: this.initialTime.seconds,
+      elapsed: null,
+      remainingTime: null,
     };
+    this.initialTime = { minutes: 25, seconds: 0 };
+    this.duration = this.calculateDuration(this.initialTime);
+    this.startTime = null;
+    this.timeout = null;
     this.onTick = onTick;
-    this.interval = null;
+  }
+
+  calculateDuration({ minutes, seconds }) {
+    return (Number(minutes) * 60 + Number(seconds)) * 1000;
+  }
+
+  formatTime() {
+    let totalSeconds = Math.floor(this.state.remainingTime / 1000);
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+
+    return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(
+      2,
+      "0"
+    )}`;
+  }
+
+  tick() {
+    const elapsed = Date.now() - this.startTime;
+    this.state.remainingTime = this.duration - elapsed;
+
+    if (this.state.remainingTime <= 0) {
+      this.state.running = false;
+    }
+    this.onTick(this.formatTime(), this.state.remainingTime, this.duration);
+
+    const delay = 1000 - (elapsed % 1000);
+    this.timeout = setTimeout(() => {
+      this.tick();
+    }, delay);
   }
 
   start() {
     if (this.state.running) return;
 
+    this.startTime = Date.now();
+
     this.state.running = true;
-
-    this.interval = setInterval(() => {
-      if (this.state.seconds === 0) {
-        if (this.state.minutes === 0) {
-          clearInterval(this.interval);
-          this.state.running = false;
-          return;
-        }
-        this.state.minutes--;
-        this.state.seconds = 59;
-      } else {
-        this.state.seconds--;
-      }
-
-      this.onTick(
-        this.initialTime.minutes,
-        this.initialTime.seconds,
-        this.state.minutes,
-        this.state.seconds
-      );
-    }, 1000);
+    this.tick();
   }
 
   pause() {
-    clearInterval(this.interval);
+    clearTimeout(this.timeout);
     this.state.running = false;
   }
 
   reset() {
-    clearInterval(this.interval);
+    clearTimeout(this.timeout);
     this.state = {
       running: false,
       minutes: 25,
@@ -63,14 +74,10 @@ class UIController {
     this.timerCircle = document.getElementById("timer-circle");
     this.controlBtn = document.getElementById("control-button");
 
-    this.timer = new Timer(
-      (initialMinutes, initialSeconds, minutes, seconds) => {
-        const formattedMinutes = String(minutes).padStart(2, "0");
-        const formattedSeconds = String(seconds).padStart(2, "0");
-        this.timerLabel.textContent = `${formattedMinutes}:${formattedSeconds}`;
-        this.updateCircle(initialMinutes, initialSeconds, minutes, seconds);
-      }
-    );
+    this.timer = new Timer((timerStr, remainingTime, initialDuration) => {
+      this.timerLabel.textContent = timerStr;
+      this.updateCircle(remainingTime, initialDuration);
+    });
   }
 
   setupEventListeners() {
@@ -98,12 +105,10 @@ class UIController {
     this.timerLabel.textContent = "25:00";
   }
 
-  updateCircle(initialMinutes, initialSeconds, minutes, seconds) {
-    const initialTime = initialMinutes * 60 + initialSeconds;
-    let secondsLeft = minutes * 60 + seconds;
+  updateCircle(remainingTime, initialDuration) {
     this.timerCircle.setAttribute(
       "stroke-dasharray",
-      `${(767 / initialTime) * secondsLeft} 1000`
+      `${(767 / initialDuration) * remainingTime} 1000`
     );
   }
 }
